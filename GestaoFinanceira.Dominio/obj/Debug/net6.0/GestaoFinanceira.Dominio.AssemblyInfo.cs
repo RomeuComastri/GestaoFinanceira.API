ffbb05a5f796bc7515cc1f82b595968f2d648517
romeu.comastri <comastriromeu@gmail.com>
2025-01-30 23:36:06 -0300

    End points gestão de finanças

--- GestaoFinanceira.Dominio/obj/Debug/net6.0/GestaoFinanceira.Dominio.AssemblyInfo.cs
+++ GestaoFinanceira.Dominio/obj/Debug/net6.0/GestaoFinanceira.Dominio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoFinanceira.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d591ce18926e1211b403a9a66e00cf3843bebca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775b75c29f9fe193727a4ceca52371d2022f1283")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoFinanceira.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoFinanceira.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
